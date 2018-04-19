73e1d51c32b85ae0226f73a19c4d5e5998299c17
Brian Thorne <brian@thorne.link>
2018-04-19 11:39:47 +1000

    Start work on schemas for Projects

--- backend/schemas/project.py
+++ backend/schemas/project.py
@@ -6,8 +6,24 @@ class SchemaSchema(Schema):
 
 
 class NewProjectSchema(Schema):
-
     name = fields.Str()
+    notes = fields.Str()
+    number_parties = fields.Integer()
     result_type = fields.Str()
-
     schema = fields.Nested(SchemaSchema())
+
+
+class ProjectCreationResponse(Schema):
+    project_id = fields.Str()
+    update_tokens = fields.List(fields.Str())
+    result_token = fields.Str()
+
+
+class ProjectDescription(NewProjectSchema):
+    project_id = fields.Str()
+
+
+class ProjectListSummary(Schema):
+    project_id = fields.Str()
+    time_added = fields.DateTime()
+
